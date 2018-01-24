9b8d3d8076f098017fcca06143cfea4ef2b745ba
sentientwaffle <sentientwaffle@gmail.com>
2018-01-24 10:31:16 -0800

    fix: Packet.parse() shouldn't return undefined
    If the packet is invalid, throw the parse error instead of:
    
        TypeError: Cannot match against 'undefined' or 'null'.
    
    when using the return value.

--- src/utils/packet.js
+++ src/utils/packet.js
@@ -1,19 +1,13 @@
 'use strict'
 
 const IlpPacket = require('ilp-packet')
-const debug = require('debug')('ilp:packet')
 
 const serialize = (p) => {
   return IlpPacket.serializeIlpPayment(p)
 }
 
 const parse = (packet) => {
-  try {
-    return IlpPacket.deserializeIlpPayment(Buffer.from(packet, 'base64'))
-  } catch (e) {
-    debug('error while parsing packet: ' + e.message)
-    return undefined
-  }
+  return IlpPacket.deserializeIlpPayment(Buffer.from(packet, 'base64'))
 }
 
 module.exports = {
