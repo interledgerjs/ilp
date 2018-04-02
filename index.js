0de7476157210f14a3b1fe0d8b6133d76740ed4a
Michiel de Jong <michiel@ripple.com>
2018-04-02 17:52:19 +0200

    test: see if this makes integration tests pass

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   ILQP: require('./src/lib/ilqp'),
-  LT: require('./src/lib/lt'),
   PSK: require('./src/lib/psk'),
   IPR: require('./src/lib/ipr'),
   SPSP: require('./src/lib/spsp')
