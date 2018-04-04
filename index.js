3fd5443afbdf33911b6225f6f19a0de7f6365de3
Michiel de Jong <michiel@ripple.com>
2018-04-04 09:03:34 +0200

    chore: require ilp-protocol-lt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   ILQP: require('./src/lib/ilqp'),
-  LT: require('./src/lib/lt'),
+  LT: require('ilp-protocol-lt'),
   PSK: require('./src/lib/psk'),
   IPR: require('./src/lib/ipr'),
   SPSP: require('./src/lib/spsp')
